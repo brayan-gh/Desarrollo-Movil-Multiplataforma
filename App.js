49688737d6748c162dea000f265ad801618e7acb
brayan-gh <116594063+brayan-gh@users.noreply.github.com>
2024-02-07 20:15:14 -0600

    PRACTICA DISEÑO CALCULADORA REACT NATIVE

--- App.js
+++ App.js
@@ -1,160 +1,129 @@
-import React, { useState } from "react";
-const secondaryColor = "#ffffff";
-import {
-  StyleSheet,
-  View,
-  Text,
-  Button,
-  TextInput,
-  Image,
-} from "react-native";
+import React from 'react';
+import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
 
-const App = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
+export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        {
+      <Text style={styles.display}>0</Text>
+      <View style={styles.buttons}>
+        <View style={styles.row}>
+          <TouchableOpacity style={[styles.button, styles.clearButton]}>
+            <Text style={[styles.buttonText, styles.clearButtonText]}>C</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.operatorButton]}>
+            <Text style={styles.buttonText}>/</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.operatorButton]}>
+            <Text style={styles.buttonText}>%</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.operatorButton]}>
+            <Text style={styles.buttonText}>CE</Text>
+          </TouchableOpacity>
           
-        }
-        <Image
-          source={require("./assets/facebook-logo.png")}
-          alignSelf="center"
-          style={styles.logo}
-        />
-       
-
-      </View>
-
-      <View style={styles.inputs}>
-        <TextInput
-          style={styles.input}
-          placeholder="Celular o correo electrónico"
-          value={email}
-          onChangeText={setEmail}
-          marginBottom={20}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Contraseña"
-          value={password}
-          onChangeText={setPassword}
-        />
-        
-      </View>
-    <View style={styles.button}>
-    <Button
-        title=""
-        titleColor="#ffffff"
-        
-        onPress={() => {
-        }}
-      />
-      <Text style={styles.textBu}>Iniciar Sesión</Text>
-       
-    </View>
-    
-     <View style={styles.TextOv}>
-     <Text style={styles.forgotPassword}>¿Olvidaste tu contraseña?</Text>
-     </View>
-       
-      <View style={styles.createAccountButton}> 
-      <Button
-        title="Crear cuenta"
-        onPress={() => {
-        }}
-        style={styles.createAccountButton}
-      />
-      <Image
-          source={require("./assets/meta-logo.png")}
-          alignSelf="center"
-          style={styles.logoM}
-        />
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity style={[styles.button, styles.operatorButton]}>
+            <Text style={styles.buttonText}>*</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>7</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>8</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>9</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity style={[styles.button, styles.operatorButton]}>
+            <Text style={styles.buttonText}>-</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>4</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>5</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>6</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity style={[styles.button, styles.operatorButton]}>
+            <Text style={styles.buttonText}>+</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>2</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>3</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+        <TouchableOpacity style={[styles.button, styles.operatorButton]}>
+            <Text style={styles.buttonText}>=</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>.</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.doubleButton]}>
+            <Text style={styles.buttonText}>0</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#ffffff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#1f1f1f',
   },
-  header: {
-    height: 100,
-    backgroundColor: "linear-gradient(#3b5998, #447b2)",
-    alignItems: "center",
-    justifyContent: "center",
+  display: {
+    marginTop: 150,
+    padding: 50,
+    backgroundColor: '#np333',
+    textAlign: 'right',
+    fontSize: 50,
+    color: '#fff',
   },
-  logo: {
-    width: 60,
-    height: 60,
-  marginTop: 140,
+  buttons: {
+    marginTop: 40,
+    paddingBottom: 35,
   },
-  textBu:{
-    marginTop: -30,
-    color: "white",
-    fontSize: 20,
-  },
-  
-  logoM: {
-    marginTop: 20,
-    width: 120,
-    height: 50,
-  },
-  createAccountButton: {
-    width: 300,
-    height: 40,
-    borderRadius: 30,
-    borderColor: "#4267b2",
-    borderWidth: 2,
-    backgroundColor: "#ffffff",
-    color: "#4267b2",
-    marginTop: 160,
-  },
-  title: {
-    alignSelf:"center",
-    marginTop:20
-  },
-  inputs: {
-    alignSelf:"center",
-    width: 350,
-    marginVertical: 20,
-    marginTop:150
-  },
-  input: {
-    borderColor: "#000",
-    borderWidth: 0.5,
-    height: 53,
-    padding: 10,
-    borderRadius: 15,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.3,
-    shadowRadius: 5,
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 10,
   },
   button: {
-    width: 350,
-    height: 45,
-    borderRadius: 30,
-    borderColor: "#000",
-    borderWidth: 0,
-    backgroundColor: "#1567d9",
-    alignItems: "center",
-    justifyContent: "center",
-    fontSize: 16,
-    fontWeight: "bold",
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#2c2c2c',
+    padding: 20,
+    borderRadius: 50,
+    width: 80,
+    height: 80,
   },
-  TextOv:{
-    marginTop:30,
-  }
+  doubleButton: {
+    width: 184,
+  },
+  buttonText: {
+    fontSize: 30,
+    color: '#fff',
+  },
+  clearButton: {
+    backgroundColor: '#ff4d4d',
+  },
+  clearButtonText: {
+    color: '#fff',
+  },
+  operatorButton: {
+    backgroundColor: '#f0ad4e',
+  },
 });
-
-export default App;
